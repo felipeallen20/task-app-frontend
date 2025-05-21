ec864edd43388baa2ccc94a13f45269adf3de00a
Felipe Gutierrez Romero <felipegutiromero@gmail.com>
2025-05-21 10:45:41 -0500

    feat(task-counter): generate task counter component

---
+++ src/app/components/task/task-counter/task-counter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-counter',
+  imports: [],
+  templateUrl: './task-counter.component.html',
+  styleUrl: './task-counter.component.css'
+})
+export class TaskCounterComponent {
+
+}
