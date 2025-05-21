afbc281413eb1680d9ade466ce13ae7b59241cb5
Felipe Gutierrez Romero <felipegutiromero@gmail.com>
2025-05-21 11:21:46 -0500

    chore(app): add import for task count component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskCounterComponent } from './components/task/task-counter/task-counter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TaskCounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
