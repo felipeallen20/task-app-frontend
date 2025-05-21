39195f82c5b498d008c0f602c758c63e801fabc2
Felipe Gutierrez Romero <felipegutiromero@gmail.com>
2025-05-21 11:11:58 -0500

    chore(task-count-box): add import for mat-card component from angular material

--- src/app/components/task/task-count-box/task-count-box.component.ts
+++ src/app/components/task/task-count-box/task-count-box.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-task-count-box',
   templateUrl: './task-count-box.component.html',
-  styleUrls: ['./task-count-box.component.css']
+  styleUrls: ['./task-count-box.component.css'],
+  imports: [MatCardModule],
 })
 export class TaskCountBoxComponent {
   @Input() title: string = '';
